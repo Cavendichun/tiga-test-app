440354a0471bc13d28b6cb516d88c63aa6d3351a
lichun01 <lichun01@qianxin.com>
2025-03-20 18:35:14 +0800

    init: init

--- container/awesome-container/src/main.js
+++ container/awesome-container/src/main.js
@@ -1,1 +1,19 @@
-export default {};
+export default {
+  layout: {
+    // type: 'horizontal', // horizontal vertical mix
+    // 自定义menu加载器
+    // menuLoader() {
+    //   return [];
+    // },
+    menuFilter(node) {
+      console.log(node);
+      return true
+    },
+  },
+  hooks: {
+    // 加载完project 配置后，可以对projectConfig进行修改
+    afterLoadProjectConfig(projectConfig) {
+      projectConfig.haha = '12345';
+    },
+  },
+};
